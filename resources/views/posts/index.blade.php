2b7ee89c2b89d5b42dee70e4827915cbf4987d93
lks88 <lucyshields17@gmail.com>
2023-06-11 11:16:37 +0100

    -Edit button added -Colours changed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-slate-800 overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="max-w-7xl mx-auto p-6 lg:p-8">
                     <div class="flex justify-center">
                     </div>
@@ -29,7 +29,13 @@
                         </div>
 
                         <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                            @if($can_edit == false)
                             {{$user->name}}
+                            @elseif($can_edit == true)
+                                <x-button class="ml-4">
+                                    {{ __('Edit Post') }}
+                                </x-button>
+                            @endif
                         </div>
                     </div>
                 </div>
