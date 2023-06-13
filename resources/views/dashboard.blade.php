9b4b74c6a8516827b31d44cb19ca38606c3e24a4
lks88 <lucyshields17@gmail.com>
2023-06-13 22:30:56 +0100

    -fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Blog Posts') }}
         </h2>
     </x-slot>
 
@@ -15,6 +15,9 @@
                     <div class="mt-16">
 
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                            @if(!$posts)
+                                There are no posts yet, create one <a href="/create">here</a>
+                            @endif
 
                             @foreach($posts as $post)
                                 <a href="/post/{{$post->id}}" class="scale-100 p-6 bg-white  dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
@@ -31,6 +34,7 @@
                                                 {{$post->body}}
                                             </p>
                                     </div>
+
 
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -64,7 +64,6 @@
 
             </div>
         </div>
-
         @foreach($comments as $comment)
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
